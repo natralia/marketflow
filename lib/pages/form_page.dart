7dc6780cd7fb3da5685222b9ac8b9dc2cbc3b9ac
Nathalia Caetano <nathalia@lihai.com.br>
2022-12-30 00:40:51 -0300

    add form page for create product

---
+++ lib/pages/form_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
+import 'package:marketflow/models/product.dart';
+import 'package:marketflow/utils/custom_colors.dart';
+
+import '../services/cart.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _controller = TextEditingController();
+  final _moneyInputController = MoneyMaskedTextController(
+    leftSymbol: "\$",
+    decimalSeparator: ".",
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Center(
+                child: Text(
+                  "Add product",
+                  style: TextStyle(
+                    color: CustomColors.textPrimary,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.0
+                  ),
+                ),
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: (){
+                        debugPrint("${Cart.instance.listProduct()}");
+                      }, 
+                      icon: Icon(
+                        Icons.chevron_left,
+                        color: CustomColors.primary,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: (){
+                        Cart.instance.addProduct(Product(name: _controller.text, price: _moneyInputController.numberValue.toInt()));
+                      }, 
+                      icon: Icon(
+                        Icons.done,
+                        color: CustomColors.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children:  [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 6,
+                  ),
+                  child: TextFormField(
+                    controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: "Name",
+                    helperText: "Product name",
+                    border: const OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: CustomColors.primary,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.name,
+                  textCapitalization: TextCapitalization.words,
+                  ),
+                ),
+                 Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 6,
+                  ),
+                  child: TextFormField(
+                      controller: _moneyInputController,
+                    decoration: InputDecoration(
+                      labelText: "Price",
+                      helperText: "Product price",
+                      border: const OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: CustomColors.primary,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
