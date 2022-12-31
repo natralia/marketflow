ae3375d94a6fe17b893c4550d7e9006c79a55dc9
Nathalia Caetano <nathalia@lihai.com.br>
2022-12-31 13:34:13 -0300

    add cart animation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
-import '../models/product.dart';
 import '../services/cart.dart';
 import '../utils/custom_colors.dart';
 import '../widgets/app_bar_widget.dart';
